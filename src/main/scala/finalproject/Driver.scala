d785370f030ae6a9f67083280334874040df8797
Justin <justintomas783@gmail.com>
2018-03-18 16:37:31 -0700

    Finished MostCommonEmergencyByZIP func

--- src/main/scala/finalproject/Driver.scala
+++ src/main/scala/finalproject/Driver.scala
@@ -7,14 +7,31 @@ import org.apache.spark.rdd._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import scala.collection._
+import scala.collection.immutable.ListMap
 
 object Driver extends App {
-  Logger.getLogger("org").setLevel(Level.OFF)
-  Logger.getLogger("akka").setLevel(Level.OFF)
 
-  val conf = new SparkConf().setAppName("FinalProject").setMaster("local[4]")
-  val sc = new SparkContext(conf)
+//  def emergencyByZip() = {
 
-  val lines = sc.textFile("911.csv")
-  lines.foreach(println)
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val conf = new SparkConf().setAppName("FinalProject").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+
+    val lines = sc.textFile("911.csv")
+
+    val myTuple = lines
+        .map(_.split(","))
+        .map(x => (x(3), x(4)))
+        .persist();
+
+    // val accidentGroupings =  myTuple.reduceByKey((x, y) => x + ", " + y)
+
+    val accidentCounts = myTuple.countByValue()
+    val sortByKey = ListMap(accidentCounts.toSeq.sortBy(x => (x._1.toString().substring(1, 6), x._2)): _*)
+    sortByKey.foreach {
+      println(_)
+    };
+//  }
 }
